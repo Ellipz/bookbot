d654674c87b0a3c26da3040e37fd5e133dd756e8
Ellipz <n.lercier@gmail.com>
2024-11-11 22:48:32 +0100

    1.2_Add dict_to_listofdict and sort_on  function + report printer to main.py

--- main.py
+++ main.py
@@ -3,9 +3,24 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(book_path)
     per_character_count = get_per_character_count(text)
+    lod_per_character_count=dict_to_listofdict(per_character_count)
+    
     #print(text)
     #print(word_count)
-    print(per_character_count)
+    #print(per_character_count)
+    #print(lod_per_character_count)
+
+    lod_per_character_count.sort(reverse=True, key=sort_on)
+
+    #print(lod_per_character_count)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    print(" ")
+    for d in lod_per_character_count:
+        print(f"The '{d["char"]}' character was found {d["count"]} times")
+    print("--- End report ---")
+
 
 
 def get_book_text(path):
@@ -24,5 +39,19 @@ def get_per_character_count(text):
         else:
             character_set[c] = 1
     return character_set
+
+def dict_to_listofdict(dict):
+    listofdict=[]
+    for c in dict:
+        if c.isalpha():
+            listofdict.append({"char":c ,"count":dict[c]})
+    return listofdict
+
+def sort_on(dict):
+    return dict["count"]
+
+#def sort_of_dicts(dict)
+    
+    #dict.sort(reverse=True, key=sort_on)
 
 main()
